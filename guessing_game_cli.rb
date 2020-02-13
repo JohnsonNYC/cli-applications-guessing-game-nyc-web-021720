592c8b4b423daaba57ab9b7c1729708aa11c7a97
Johnson Kow <jkow95@gmail.com>
2020-02-13 17:04:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 
 def run_guessing_game
-  numb = rand(6) + 1
- input = gets.chomp
+  numb = rand(6) + 1 #produced as integer or float
+ input = gets.chomp  #captured as a string
  
-  if input == numb.to_s
+  if input == numb.to_s #needs to be string because input is string
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
