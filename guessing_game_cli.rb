9cad89f4e87b897dc388214c411fd5fcc2a94935
Johnson Kow <jkow95@gmail.com>
2020-02-13 16:55:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,8 @@
 def run_guessing_game
   numb = rand(6) + 1
  input = gets.chomp
-  
+  if input == numb 
+    puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
   elsif input != numb
