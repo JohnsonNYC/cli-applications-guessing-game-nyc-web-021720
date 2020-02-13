0f3ce51c8ab12f1f05f51b3bf1fa457db3d61e72
Johnson Kow <jkow95@gmail.com>
2020-02-13 17:01:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ require 'pry'
 def run_guessing_game
   numb = rand(6) + 1
  input = gets.chomp
- binding.pry
+ 
   if input == numb 
     puts "You guessed the correct number!"
   elsif input == "exit"
