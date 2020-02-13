ac14beffc18f958bd0285b629e49b59c068692ca
Johnson Kow <jkow95@gmail.com>
2020-02-13 16:54:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,11 +2,11 @@
 def run_guessing_game
   numb = rand(6) + 1
  input = gets.chomp
-  if input == "exit"
+  elsif input == "exit"
     puts "Goodbye!"
   elsif input != numb
     puts "Sorry! The computer guessed #{numb}."
-  elsif input == numb 
+  if input == numb 
     puts "You guessed the correct number!"
   end 
 end 
