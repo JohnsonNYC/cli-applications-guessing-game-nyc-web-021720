c9625841640f4a1931a79bbea7f5877f55bc34e5
Johnson Kow <jkow95@gmail.com>
2020-02-13 16:52:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,11 @@
 # Code your solution here!
+def run_guessing_game
+  numb = rand(6) + 1
+ input = gets.chomp
+  if input == "exit"
+    puts "Goodbye!"
+  end 
+  if input != numb
+    puts "Sorry! The computer guessed #{numb}."
+  end 
+end 
